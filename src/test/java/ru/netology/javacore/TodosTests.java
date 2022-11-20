8631dc1c5145b16f9604931e6f786a8af5b23fda
Yuriy Sviridov <crip.shooter@gmail.com>
2022-11-20 13:22:11 +0300

    add tests

--- src/test/java/ru/netology/javacore/TodosTests.java
+++ src/test/java/ru/netology/javacore/TodosTests.java
@@ -1,6 +1,41 @@
 package ru.netology.javacore;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class TodosTests {
 
+    Todos todos = new Todos();
+
     // ваши тесты для класса Todos
+    @Test
+    void testSortedAndUniqueTasks() {
+        todos.addTask("Работа");
+        todos.addTask("Отдых");
+        todos.addTask("Сон");
+        todos.addTask("Работа");
+        Assertions.assertEquals("Отдых Работа Сон", todos.getAllTasks());
+    }
+
+    @Test
+    void testMaxTasks() {
+        todos.addTask("Работа");
+        todos.addTask("Отдых");
+        todos.addTask("Сон");
+        todos.addTask("Пища");
+        todos.addTask("Рисование");
+        todos.addTask("Футбол");
+        todos.addTask("Компьютерные игры");
+        todos.addTask("Тренажерный зал");
+        todos.addTask("Поход в музей");
+        Assertions.assertEquals("Компьютерные игры Отдых Пища Работа Рисование Сон Футбол", todos.getAllTasks());
+    }
+
+    @Test
+    void removeTask() {
+        todos.addTask("Рисование");
+        todos.addTask("Футбол");
+        todos.removeTask("Рисование");
+        Assertions.assertEquals("Футбол", todos.getAllTasks());
+    }
 }
